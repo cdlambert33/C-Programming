b24b7dd05daa82152d2a0451532b54334c33b431
Caston Lambert <cdlambert33@yahoo.com>
2019-01-22 16:42:41 -0600

    Finsihed integer rollover code

---
+++ rolloverc/rollover.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+
+	printf("Hello World!\n");
+	int sumOfInts = 1;
+	while(0<sumOfInts){
+		sumOfInts = sumOfInts + 1;
+
+		if(0 == (sumOfInts%1000)){
+			printf("%d\n", sumOfInts);
+		}	
+	}
+	printf("%d\n", sumOfInts);
+
+	printf("Bye world\n");
+	return 0;
+}
