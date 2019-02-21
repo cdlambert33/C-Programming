1007be7f7f56f21ec24efebd8c25fd36b6c227b8
Caston Lambert <cdlambert33@yahoo.com>
2019-02-21 10:48:15 -0600

    added a few more methods. Still have core dump error

--- AddingArrayRow-wiseAndColumn-wise/AddingRowsAndColumns.c
+++ AddingArrayRow-wiseAndColumn-wise/AddingRowsAndColumns.c
@@ -5,17 +5,24 @@
 void helloWorld();
 void createArray();
 void fillArray();
+void emptyArray();
+void findArraySumColumnwise();
+void findArraySumRowwise();
 
 // Variable declaration
 int * array;
-int arraySize = 10;
+int arraySize = 3;
 double * arrayAddress;
+int rowIndex, columnIndex;
+int columnArraySum, rowArraySum;
 
 
 int main()
 {
 	helloWorld();
 	//createArray();
+	//findArraySumColumnwise();
+	findArraySumRowwise();
 
 	for(int i = 2; i < 6; i++){
 		printf("I is %d\n", i);
@@ -34,14 +41,14 @@ void helloWorld(){
 void createArray(){
 	array = (int*)malloc(arraySize * arraySize * sizeof(int));
 
-	//zeroOutArray();
+	emptyArray();
 	fillArray();
 	//printf("%p\n", array);
 }
 
 void fillArray(){
 	int counter = 1;
-	int rowIndex, columnIndex;
+	//int rowIndex, columnIndex;
 
 	for(rowIndex = 0; rowIndex < arraySize; rowIndex++){
 		for(columnIndex = 0; columnIndex < arraySize; columnIndex++){
@@ -49,4 +56,34 @@ void fillArray(){
 			counter++;
 		}
 	}
+}
+
+void emptyArray(){
+	for(rowIndex = 0; rowIndex < arraySize; rowIndex++){
+		for(columnIndex = 0; columnIndex < arraySize; columnIndex++){
+			*(arrayAddress + columnIndex + (rowIndex * arraySize)) = 0;
+		}
+	}
+}
+
+void findArraySumColumnwise(){
+	columnArraySum = 0;
+
+	for(columnIndex = 0; columnIndex < arraySize; columnIndex++){
+		for(rowIndex = 0; rowIndex < arraySize; rowIndex++){
+			columnArraySum = columnArraySum + (*(arrayAddress + columnIndex + (rowIndex * arraySize)));
+		}
+	}
+	printf("%.0d\n", columnArraySum );
+}
+
+void findArraySumRowwise(){
+	rowArraySum = 0;
+
+	for(rowIndex = 0; rowIndex < arraySize; rowIndex++){
+		for(columnIndex = 0; columnIndex < arraySize; columnIndex++){
+			rowArraySum = rowArraySum + (*(arrayAddress + rowIndex + (columnIndex * arraySize)));
+		}
+	}
+	printf("%.0d\n", rowArraySum );
 }
