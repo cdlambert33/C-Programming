eca1ff49a682e0090d8eea47e04b0274cadc5ded
Caston Lambert <cdlambert33@yahoo.com>
2019-04-17 17:15:42 -0500

    working, commented out edits

--- DeterminantC/Determinant.c
+++ DeterminantC/Determinant.c
@@ -1,4 +1,4 @@
-
+/*
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -43,3 +43,32 @@ int main(int argc, char *argv[])
         fclose(datafile);
     }
 }
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define ARRAYSIZE 16
+
+int main(int argc, char *argv[])
+{
+    char f_name[50];
+    double a[ARRAYSIZE][ARRAYSIZE];
+    int i,j;
+    double det;
+    //Create filename
+    sprintf(f_name,"m0016x0016.bin");
+    printf("Reading array file %s of size %dx%d\n",f_name,ARRAYSIZE,ARRAYSIZE);
+    //Open file
+    FILE *datafile=fopen(f_name,"rb");
+    //Read elelements
+    for (i=0; i< ARRAYSIZE; i++)
+        for (j=0; j< ARRAYSIZE; j++)
+        {
+            fread(&a[i][j],sizeof(double),1,datafile);
+            printf("a[%d][%d]=%f\n",i,j,a[i][j]);
+        }
+            printf("Matrix has been read.\n");
+}
+
